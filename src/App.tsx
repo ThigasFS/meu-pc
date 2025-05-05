4702dd049ce33b558d5150d4f1e8e2bc196ef7c2
ThigasFS <thigerwoods71@gmail.com>
2025-05-05 16:24:07 -0300

    0.1

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho"
+
+function App() {
+  return (
+    <>
+      <Cabecalho />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,9 @@
+import './Cabecalho.module.css'
+
+function Cabecalho() {
+  return (
+    <div className='titulo'>Meu PC</div>
+  )
+}
+
+export default Cabecalho
